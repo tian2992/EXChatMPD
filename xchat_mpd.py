020a1313db3cae36b6452a0652ec5a60b193385c
Sebastian Oliva <tian2992@gmail.com>
2011-03-13 18:56:26 -0600

    re factored command infrastructure

--- xchat_mpd.py
+++ xchat_mpd.py
@@ -18,27 +18,44 @@ import xchat
 import mpd
 
 __module_name__ = "MPD Status"
-__module_version__ = "0.2"
+__module_version__ = "0.3"
 __module_description__ = "MPD Current playing song"
 
-def sendStatus(word, word_eolm, userdata):
-  client = mpd.MPDClient()
+
+def send_status():
   try:
-    client.connect("localhost",6600) #TODO: parametrize
     current_song = client.currentsong()
-    client.disconnect()
   except:
     current_song = {}
   if current_song == {}:
-     status = "silence"
+    status = "silence"
   else:
-     try:
-       status = current_song["title"] + " - " + current_song["artist"]
-       if "album" in current_song:
-         status = status +  " (" + current_song["album"] + ")"
-     except:
-       status = "a song with missing tags"
+    try:
+      status = current_song["title"] + " - " + current_song["artist"]
+      if "album" in current_song:
+        status = status +  " (" + current_song["album"] + ")"
+    except:
+      status = "a song with missing tags"
   xchat.command("me is listening to %s" % (status))
+
+def command_pause():
+  client.pause()
+
+def command_set_volume(argString):
+  volume = int(argString)
+  client.setvol(volume)
+
+def EXChatMPD(word, word_eol, userdata):
+  client = mpd.MPDClient()
+  try:
+    client.connect("localhost",6600) #TODO: parametrize
+    if (word_eol[0]=="mpd"):
+      send_status()
+    else: #it's not status
+      
+    client.disconnect()
+  except: #fail to connect
+    print("connection failed")
   return xchat.EAT_ALL
   
-xchat.hook_command("mpd", sendStatus, help="/mpd MPD Current playing song")
+xchat.hook_command("mpd", EXChatMPD, help="/mpd command or just /mpd for current playing song")
