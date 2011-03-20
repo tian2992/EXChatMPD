524fbca469989414c95acbec69fd3daa6dc13111
Sebastian Oliva <tian2992@gmail.com>
2011-03-20 12:51:27 -0600

    fixed routes of install script

--- install_script.sh
+++ install_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p ~/.xchat2/plugins
-cp xchat_mpd.py ~/.xchat2/plugins/
-cp mpd.py ~/.xchat2/plugins/
+#mkdir -p ~/.xchat2/plugins
+cp xchat_mpd.py ~/.xchat2/
+#cp mpd.py ~/.xchat2/plugins/
 
